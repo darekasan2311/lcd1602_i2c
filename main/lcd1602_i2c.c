7f8476eafd566571b84196647f414751d656624d
bcds <abcxyz>
2025-09-25 14:40:57 +0900

    print funcs

--- main/lcd1602_i2c.c
+++ main/lcd1602_i2c.c
@@ -1,60 +1,71 @@
 #include <stdio.h>
+#include <stdarg.h>
 
 #include "driver/i2c_master.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "esp_log.h"
 
-
 #define I2C_MASTER_SCL_IO       22
 #define I2C_MASTER_SDA_IO       21
 #define I2C_MASTER_FREQ_HZ      100000
-#define LCD_I2C_ADDRESS         0x27    // I2C address of LCD 
+#define LCD_I2C_ADDRESS         0x27 // I2C address of LCD
 
 // LCD Commands
 #define LCD_CLEARDISPLAY        0x01
 #define LCD_CURSORRETURN        0x02
 #define LCD_INPUTSET            0x04
-// #define LCD_DISPLAYCONTROL      0x08
-// #define LCD_CURSORSHIFT         0x10
-// #define LCD_FUNCTIONSET         0x20
-// #define LCD_SETCGRAMADDR        0x40
-// #define LCD_SETDDRAMADDR        0x80
+#define LCD_SET_DDRAM_ADR       0x80
 
-// // Flags for display entry mode
-// #define LCD_ENTRYRIGHT          0x00
-// #define LCD_ENTRYLEFT           0x02
-// #define LCD_ENTRYSHIFTINCREMENT 0x01
-// #define LCD_ENTRYSHIFTDECREMENT 0x00
+// Rows
+#define LCD_ROW1                0x00
+#define LCD_ROW2                0x40
 
-// // Flags for display on/off control
-// #define LCD_DISPLAYON           0x04
-// #define LCD_DISPLAYOFF          0x00
-// #define LCD_CURSORON            0x02
-// #define LCD_CURSOROFF           0x00
-// #define LCD_BLINKON             0x01
-// #define LCD_BLINKOFF            0x00
-
-// // Flags for function set
-// #define LCD_8BITMODE            0x10
-// #define LCD_4BITMODE            0x00
-// #define LCD_2LINE               0x08
-// #define LCD_1LINE               0x00
-// #define LCD_5x10DOTS            0x04
-// #define LCD_5x8DOTS             0x00
+// LCD buffer
+#define LCD_BUFFERSIZE          64
 
 // Flags for backlight control
 #define PIN_BACKLIGHT           0x0C
 #define PIN_NOBACKLIGHT         0x00
 
-#define PIN_ENABLE              0x04  // Enable bit
-#define PIN_RW                  0x02  // Read/Write bit
-#define PIN_RS                  0x01  // Register select bit
+#define PIN_ENABLE              0x04     // Enable bit
+#define PIN_RW                  0x02     // Read/Write bit
+#define PIN_RS                  0x01     // Register select bit
 
 i2c_master_dev_handle_t dev_handle;
 i2c_master_bus_handle_t bus_handle;
 
 static uint8_t state_backlight = PIN_BACKLIGHT; // DEFAULT ON
+
+
+void i2c_init(void)
+{
+    i2c_master_bus_config_t i2c_mst_config = {
+        .clk_source = I2C_CLK_SRC_DEFAULT,
+        .i2c_port = I2C_NUM_0,
+        .scl_io_num = I2C_MASTER_SCL_IO,
+        .sda_io_num = I2C_MASTER_SDA_IO,
+        .glitch_ignore_cnt = 7,
+        .flags.enable_internal_pullup = true,
+    };
+
+    ESP_ERROR_CHECK(i2c_new_master_bus(&i2c_mst_config, &bus_handle));
+
+    i2c_device_config_t dev_cfg = {
+        .dev_addr_length = I2C_ADDR_BIT_LEN_7,
+        .device_address = LCD_I2C_ADDRESS,
+        .scl_speed_hz = I2C_MASTER_FREQ_HZ,
+    };
+
+    if (i2c_master_bus_add_device(bus_handle, &dev_cfg, &dev_handle) != ESP_OK)
+    {
+        ESP_LOGE("I2C", "ADDING DEVICE FAILED");
+    }
+    else
+    {
+        ESP_LOGI("I2C", "DEVICE ADDED SUCCESSFULLY");
+    };
+}
 
 static void lcd_send_byte(uint8_t data)
 {
@@ -91,6 +102,17 @@ static void lcd_send_data(uint8_t val)
     vTaskDelay(pdMS_TO_TICKS(2));
 }
 
+static void lcd_set_cursor(uint8_t row, uint8_t col)
+{
+    uint8_t address;
+    if (row == 0)
+        address = LCD_ROW1 + col;
+    else
+        address = LCD_ROW2 + col;
+
+    lcd_send_cmd(LCD_SET_DDRAM_ADR | address);
+}
+
 void lcd_init(void)
 {
     // ---- LCD Init sequence ----
@@ -123,43 +145,32 @@ void lcd_backlight_on(void)
     lcd_send_cmd(state_backlight);
 }
 
+void lcd_print(const char *data)
+{
+    while (*data)
+    {
+        lcd_send_data(*data);
+        data++;
+    }
+}
+
+void lcd_printf(uint8_t row, uint8_t col, const char *fmt, ...)
+{
+    char buffer[LCD_BUFFERSIZE];
+
+    va_list args;
+    va_start(args, fmt);
+    vsnprintf(buffer, sizeof(buffer), fmt, args);
+    va_end(args);
+
+    lcd_set_cursor(row, col);
+    lcd_print(buffer);
+}
+
 void app_main(void)
 {
-
-    i2c_master_bus_config_t i2c_mst_config = {
-        .clk_source = I2C_CLK_SRC_DEFAULT,
-        .i2c_port = I2C_NUM_0,
-        .scl_io_num = I2C_MASTER_SCL_IO,
-        .sda_io_num = I2C_MASTER_SDA_IO,
-        .glitch_ignore_cnt = 7,
-        .flags.enable_internal_pullup = true,
-    };
-
-    ESP_ERROR_CHECK(i2c_new_master_bus(&i2c_mst_config, &bus_handle));
-
-    i2c_device_config_t dev_cfg = {
-        .dev_addr_length = I2C_ADDR_BIT_LEN_7,
-        .device_address = LCD_I2C_ADDRESS,
-        .scl_speed_hz = I2C_MASTER_FREQ_HZ,
-    };
-
-    if (i2c_master_bus_add_device(bus_handle, &dev_cfg, &dev_handle) != ESP_OK)
-    {
-        ESP_LOGE("I2C", "ADDING DEVICE FAILED");
-    }
-    else
-    {
-        ESP_LOGI("I2C", "DEVICE ADDED SUCCESSFULLY");
-    };
-
+    i2c_init();
     lcd_init();
-    lcd_send_data('H');
-    lcd_send_data('i');
-    vTaskDelay(pdMS_TO_TICKS(2000));
-    // lcd_clear();
-    lcd_backlight_off();
-    vTaskDelay(pdMS_TO_TICKS(2000));
-    lcd_backlight_on();
-    // lcd_send_cmd(0x00);
-    // ESP_ERROR_CHECK(i2c_master_transmit(dev_handle, data_wr, DATA_LENGTH, -1));
+    lcd_printf(0, 0, "Hello, world");
+    lcd_printf(1, 0, "Volt: %.2fV", 3.30);
 }
